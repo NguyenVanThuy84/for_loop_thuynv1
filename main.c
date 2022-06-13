460f8426662eb686721a916edf96d8449b6a74c3
NguyenVanThuy84 <106532457+NguyenVanThuy84@users.noreply.github.com>
2022-06-13 19:08:18 +0700

    Create main.c

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {
+	int count;
+	printf("\t This is a \n");
+	for (count = 0; count <=10; count++)
+	{
+	printf("\n \t\t\ nice \n");
+	printf (" gia tri count = %d", count);
+}
+	printf ("\n\t\t world. \n");
+	return 0;
+}
